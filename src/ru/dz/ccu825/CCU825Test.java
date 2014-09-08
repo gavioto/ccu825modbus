b5a6a418a1c6afa83d94b8011467d7c114a181f7
dmitry.zavalishin@gmail.com <dmitry.zavalishin@gmail.com@6584d901-4f6e-d804-7ded-cbcd09d7f77e>
2014-09-08 06:24:39 +0000

    rc4 test

--- src/ru/dz/ccu825/CCU825Test.java
+++ src/ru/dz/ccu825/CCU825Test.java
@@ -10,6 +10,11 @@ public class CCU825Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		// test rc4 first
+		test_rc4();
+		
+		
 		ModBusConnection mc = new ProxyModbus();
 		
 		try {
@@ -28,6 +33,25 @@ public class CCU825Test {
 		
 		
 		
+	}
+
+	private static void test_rc4() {
+		byte[] key = { 0x01, 0x02, 0x03, 0x04, 0x05 };
+		byte[] inp =  
+			{
+				(byte)0xb2, 0x39, 0x63, 0x05,  (byte)0xf0, 0x3d, (byte)0xc0, 0x27,   (byte)0xcc, (byte)0xc3, 0x52, 0x4a,  0x0a, 0x11, 0x18, (byte)0xa8
+			};
+		RC4 enc = new RC4(key );
+
+		byte[] out = enc.encrypt(inp);
+		checkzero(out);
+		//dumpBytes("enc", out);
+	}
+
+	private static void checkzero(byte[] out) {
+		for( int i = 0; i < out.length; i++ )
+			if( out[i] != 0 )
+				throw new RuntimeException("Nonzero array item at " + i);
 	}
 
 	public static void dumpBytes(String string, byte[] b) 
