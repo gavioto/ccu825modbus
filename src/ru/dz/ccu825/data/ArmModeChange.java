d22b268183278018d11a00c28c9adc9b41982ddb
dmitry.zavalishin@gmail.com <dmitry.zavalishin@gmail.com@6584d901-4f6e-d804-7ded-cbcd09d7f77e>
2014-09-15 12:46:00 +0000

    ArmModeChange and connections. no toString yet

---
+++ src/ru/dz/ccu825/data/ArmModeChange.java
@@ -0,0 +1,47 @@
+package ru.dz.ccu825.data;
+
+import java.nio.charset.Charset;
+
+public class ArmModeChange 
+{
+	static final Charset ascii = Charset.forName("ascii");
+	
+	private ArmModeChangeSource src;
+	private String ident;
+
+	public ArmModeChange(byte[] data, int startPos) {
+		//ByteBuffer bb = ByteBuffer.wrap(data, startPos, 17);
+		
+		byte type = data[startPos];
+		src = ArmModeChangeSource.forCode(type);
+		
+		ident = null;
+		
+		switch(type)
+		{
+		case 1:
+			byte[] sub = new byte[16];
+			System.arraycopy(data, startPos, sub, 0, 16);
+			ident = javax.xml.bind.DatatypeConverter.printHexBinary(sub); 
+			break;
+		case 10:
+		case 2:
+		case 3:
+		case 5:
+		case 6:
+			ident = new String( data, startPos+1, 16, ascii ).trim();
+			break;
+			
+		}
+	}
+
+	public ArmModeChangeSource getSrc() {
+		return src;
+	}
+
+	public String getIdent() {
+		return ident;
+	}
+	
+
+}
