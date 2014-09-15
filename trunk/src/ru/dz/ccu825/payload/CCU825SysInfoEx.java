8ba75a55d6fca6eb2021ac46064a6f994d61cbb1
dmitry.zavalishin@gmail.com <dmitry.zavalishin@gmail.com@6584d901-4f6e-d804-7ded-cbcd09d7f77e>
2014-09-15 09:38:15 +0000

    SysInfoEx

--- trunk/src/ru/dz/ccu825/payload/CCU825SysInfoEx.java
+++ trunk/src/ru/dz/ccu825/payload/CCU825SysInfoEx.java
@@ -13,10 +13,10 @@ import ru.dz.ccu825.util.CCU825PacketFormatException;
  */
 
 
-public class CCU825SysInfo implements ICCU825SysInfo {
+public class CCU825SysInfoEx implements ICCU825SysInfo {
 
 	
-	public static final int N_IN = 8;
+	public static final int N_IN = 16;
 	
 	
 	private final int inBits;
@@ -35,49 +35,46 @@ public class CCU825SysInfo implements ICCU825SysInfo {
 	private final double GSMBalance;
 
 
-	public CCU825SysInfo(byte [] in ) throws CCU825PacketFormatException {
+	public CCU825SysInfoEx(byte [] in ) throws CCU825PacketFormatException {
 		ByteBuffer bb = ByteBuffer.wrap(in);
 		
 		bb.order(ByteOrder.LITTLE_ENDIAN);
 		
-		if( in[0] != CCU825Packet.PKT_TYPE_SYSINFO )
-			throw new CCU825PacketFormatException("Wrong SysInfo payload header byte");
+		if( in[0] != CCU825Packet.PKT_TYPE_SYSINFO_EX )
+			throw new CCU825PacketFormatException("Wrong SysInfoEx payload header byte");
 		
-		inBits = ((int)in[1]) & 0xFF;
-		outBits = ((int)in[18]) & 0xFF;
+		inBits = bb.getShort(1);		
+		outBits = in[35];
 		
 		for( int i = 0; i < N_IN; i++ )
 		{
-			inValue [i] = ((double)bb.getShort(i+2)) * 10.0 / 4095; 
+			inValue [i] = ((double)bb.getShort(i+3)) * 10.0 / 4095; 
 		}
-
+		
 		// TODO extract as sub-object
 		{
-		byte S1 = in[19];
+		byte S1 = in[36];
 		powerOk = (S1 & 0x08) != 0;		
 		balanceValid = (S1 & 0x04) != 0;
 		}
 		
 		{
-		byte S2 = in[20];
+		byte S2 = in[37];
 		caseOpen = (S2 & 0x01) != 0;
 		}
 		
-		powerVoltage = ((double)in[21])/10.0;
+		powerVoltage = ((double)in[38])/10.0;
 		
-		batteryPercentage = in[22];
+		batteryPercentage = in[39];
 		
-		deviceTemperature = in[23];
+		deviceTemperature = in[40];
 		
-		GSMBalance = Float.intBitsToFloat( bb.getInt(24) ); 
+		GSMBalance = Float.intBitsToFloat( bb.getInt(41) ); 
 		
 	}
 
 	
 	
-	/* (non-Javadoc)
-	 * @see ru.dz.ccu825.payload.ICCU825SysInfo#toString()
-	 */
 	@Override
 	public String toString() {
 		
@@ -88,77 +85,52 @@ public class CCU825SysInfo implements ICCU825SysInfo {
 	}
 		
 
-	/* (non-Javadoc)
-	 * @see ru.dz.ccu825.payload.ICCU825SysInfo#getInBits()
-	 */
 	@Override
 	public int getInBits() {		return inBits;	}
 
 
-	/* (non-Javadoc)
-	 * @see ru.dz.ccu825.payload.ICCU825SysInfo#getOutBits()
-	 */
 	@Override
 	public int getOutBits() {		return outBits;	}
 
-	/* (non-Javadoc)
-	 * @see ru.dz.ccu825.payload.ICCU825SysInfo#getInValue()
+	/**
+	 * @return Array of input values in volts
 	 */
 	@Override
 	public double[] getInValue() {		return inValue;	}
 
 
-	/* (non-Javadoc)
-	 * @see ru.dz.ccu825.payload.ICCU825SysInfo#isPowerOk()
-	 */
 	@Override
 	public boolean isPowerOk() {		return powerOk;	}
 
 
-	/* (non-Javadoc)
-	 * @see ru.dz.ccu825.payload.ICCU825SysInfo#isBalanceValid()
-	 */
 	@Override
 	public boolean isBalanceValid() {		return balanceValid;	}
 
 
-	/* (non-Javadoc)
-	 * @see ru.dz.ccu825.payload.ICCU825SysInfo#isCaseOpen()
-	 */
 	@Override
 	public boolean isCaseOpen() {		return caseOpen;	}
 
 
-	/* (non-Javadoc)
-	 * @see ru.dz.ccu825.payload.ICCU825SysInfo#getBatteryPercentage()
-	 */
 	@Override
 	public byte getBatteryPercentage() {		return batteryPercentage;	}
 
 
-	/* (non-Javadoc)
-	 * @see ru.dz.ccu825.payload.ICCU825SysInfo#getDeviceTemperature()
-	 */
 	@Override
 	public byte getDeviceTemperature() {		return deviceTemperature;	}
 
 
-	/* (non-Javadoc)
-	 * @see ru.dz.ccu825.payload.ICCU825SysInfo#getPowerVoltage()
-	 */
 	@Override
 	public double getPowerVoltage() {		return powerVoltage;	}
 
 
-	/* (non-Javadoc)
-	 * @see ru.dz.ccu825.payload.ICCU825SysInfo#getGSMBalance()
-	 */
 	@Override
-	public double getGSMBalance() {		return GSMBalance; }
+	public double getGSMBalance() {		return GSMBalance;	}
 
 
 
 	@Override
-	public int nInputs() {		return N_IN;	}
+	public int nInputs() {
+		return N_IN;
+	}
 	
 }
